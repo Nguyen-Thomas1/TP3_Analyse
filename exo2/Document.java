8684a870781e92935e59ef901baa712a7ecf4342
Nguyen_Thomas <darkxufi@gmail.com>
2026-09-24 17:00:16 +0200

    Création du fichier Document.java

---
+++ exo2/Document.java
@@ -0,0 +1,9 @@
+public abstract class Document{
+  private int numero;
+  private string titre;
+}
+public class Document(int numero,string titre){
+  this.numero = numero;
+  this.titre = titre;
+}
+
