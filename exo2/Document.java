c978829df80f991c8a0d79e51e82ee45a8a996c3
Nguyen_Thomas <darkxufi@gmail.com>
2026-09-24 17:03:06 +0200

    Fix constructor syntax in Document class

--- exo2/Document.java
+++ exo2/Document.java
@@ -1,9 +1,10 @@
 public abstract class Document{
   private int numero;
   private string titre;
-}
-public class Document(int numero,string titre){
-  this.numero = numero;
-  this.titre = titre;
-}
+  
+    public class Document(int numero,string titre){
+    this.numero = numero;
+    this.titre = titre;
+  }
 
+}
